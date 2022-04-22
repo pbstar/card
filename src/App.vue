ec209f6e427c944dbea285949db266d7cedf90f3
pengbo <pbstar@sina.cn>
2022-04-22 16:57:25 +0800

    nopc

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
-  <router-view />
+  <div style="height:100%">
+    <router-view v-if="isMobile" />
+    <Pcindex v-if="!isMobile" />
+  </div>
+
 </template>
+<script>
+import Pcindex from './components/pcindex.vue'
+export default {
+  data () {
+    return {
+      isMobile: false
+    }
+  },
+  components: {
+    Pcindex
+  },
+  created () {
+    this.isMobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+  }
+}
+</script>
 <style>
 * {
   margin: 0;
---
+++ src/components/pcindex.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    此系统暂不支持pc端，请在移动端打开！
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
